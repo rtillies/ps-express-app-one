60c578d213966bb483fb53ee5625d68931376ba6
Richard T <74796181+rtillies@users.noreply.github.com>
2024-03-08 13:01:27 -0500

    Add routes with parameters

--- index.js
+++ index.js
@@ -2,6 +2,42 @@ const express = require('express')
 const app = express();
 const port = 3000; // usually Windows 3000, Mac 5000
 
+// ROUTES
+app.get('/', (req, res) => {
+  res.send("Hello Express!")
+})
+
+app.get('/express', (req, res) => {
+  res.send('Creating routes with Express is simple')
+})
+
+app.get('/user', (req, res) => {
+  res.send('Received a GET request from the user')
+})
+
+app.post('/user', (req, res) => {
+  res.send('Received a POST request from the user')
+})
+
+// Regular expressions
+app.get('/ab?cd', (req, res) => {
+  res.send('the question mark will get here with acd or abcd')
+})
+
+// Route Parameters
+app.get('/user/:id', (req, res) => {
+  res.send(`Navigated to user page for ${req.params.id}`)
+})
+
+app.get('/user/:id/profile', (req, res) => {
+  res.send(`Navigated to user profile page for user ${req.params.id}`)
+})
+
+app.get('/user/:id/profile/:data', (req, res) => {
+  res.send(`Navigated to user profile page for ${req.params.id} with the data ${req.params.data}`)
+})
+
+// Listen to the port
 app.listen(port, ()=> {
   console.log(`The server is listening on port ${port}`);
 })

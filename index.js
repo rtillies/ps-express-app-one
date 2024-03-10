3af4cd6187e2a4e1b984f74c8f94ec203a3e2ffd
Richard T <rtillies@gmail.com>
2024-03-10 14:48:14 -0400

    new get route

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ app.engine("perscholas", (filePath, options, callback) => {
   });
 });
 
+// specify the views directory
+// register the template engine
+app.set("views", "./views"); 
+app.set("view engine", "perscholas"); 
+
+
 
 const logReq = (req, res, next) => {
   console.log(` Request received`);
@@ -59,9 +65,29 @@ app.use((err, req, res, next) => {
 })
 
 /* ROUTES - no router needed */
-app.get('/', (req, res) => {
-  res.send("Default GET route, no router")
-})
+// app.get('/', (req, res) => {
+//   res.send("Default GET route, no router")
+// })
+app.get("/", (req, res) => {
+  const options = {
+    title: "Rendering Views with Express",
+    content:
+      "Here, we've created a basic template engine using <code>app.engine()</code> \
+      and the <code>fs</code> module, then used <code>res.render</code> to \
+      render this page using custom content within the template.<br><br> \
+      Generally, you won't want to create your own view engines, \
+      but it important to understand how they work behind the scenes. \
+      For a look at some popular view engines, check out the documentation for \
+      <a href='https://pugjs.org/api/getting-started.html'>Pug</a>, \
+      <a href='https://www.npmjs.com/package/mustache'>Mustache</a>, or \
+      <a href='https://www.npmjs.com/package/ejs'>EJS</a>. \
+      More complete front-end libraries like React, Angular, and Vue \
+      also have Express integrations.",
+  };
+
+  res.render("index", options);
+});
+
 
 // Listen to the port
 app.listen(port, () => {
